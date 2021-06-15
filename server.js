05ec47b1b238089145c3e7904d9970d466d90a85
Mwimbe1105 <gyver1105@icloud.com>
2021-06-14 19:07:31 -0600

    working code

--- server.js
+++ server.js
@@ -1,44 +1,45 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
-
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/", function (req,res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-})
+var database = require('./db/db.json')
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
 app.route("/api/notes")
-    .get(function (req,res) {
+    .get(function (req, res) {
         res.json(database);
     })
 
     .post(function (req, res) {
-        let jsonFilePath = path.join(__dirname, "/db/db.json");
+
+
 
         let newNote = req.body;
+        console.log('new notes')
+        console.log(newNote)
+        database.push(newNote)
+        res.end()
+        // let highestId = 99;
 
-        let highestId = 99;
-        
-        for (let i = 0; i < database.length; i++) {
-            let individualNote = database[i];
+        // for (let i = 0; i < database.length; i++) {
+        //     let individualNote = database[i];
 
-            if (individualNote.id > highestId) {
-                highestId = individualNote.id;
-            }
-        }
+        //     if (individualNote.id > highestId) {
+        //         highestId = individualNote.id;
+        //     }
+        // }
     })
 // require('./routes/routes')(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
-});  
+});
