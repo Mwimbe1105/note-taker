6c3291e19db01ae52da60c2e1fc98c95f0fba2ab
Mwimbe1105 <gyver1105@icloud.com>
2021-06-14 18:36:08 -0600

    working server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/", function (req,res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+app.route("/api/notes")
+    .get(function (req,res) {
+        res.json(database);
+    })
+
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+
+        let newNote = req.body;
+
+        let highestId = 99;
+        
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+                highestId = individualNote.id;
+            }
+        }
+    })
+// require('./routes/routes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
